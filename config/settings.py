e5deb564219e2c61d728a437e90c8fc0ec75a76f
Dharmateja <dharmateja2019@gmail.com>
2026-04-10 11:58:19 +0530

    made changes in workflow file and add reporting and llel tests as well

--- config/settings.py
+++ config/settings.py
@@ -2,4 +2,4 @@ import os
 
 BASE_URL = os.getenv("BASE_URL", "https://dummyjson.com")
 TIMEOUT_SECONDS = int(os.getenv("TIMEOUT_SECONDS", "5"))
-SLA_SECONDS = int(os.getenv("SLA_SECONDS", "2"))
+SLA_SECONDS = int(os.getenv("SLA_SECONDS", "1"))
